6c75c77903df825ce70c02ba091460de477676b0
Dave Stearns <dlsinfo@uw.edu>
2016-01-19 12:07:15 -0800

    use -E to preserve env when running sudo commands; remove comments from vagrantfile

--- node-intro/provision.sh
+++ node-intro/provision.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 
+# use noninteractive mode since this is automated
+export DEBIAN_FRONTEND=noninteractive
+
 # suppress erroneous error messages from dpkg-preconfigure
-sudo rm -v /etc/apt/apt.conf.d/70debconf
+sudo -E rm -v /etc/apt/apt.conf.d/70debconf
 
 # update the package database 
-sudo apt-get update
+sudo -E apt-get update
 
 # install git
-sudo apt-get install -y git
+sudo -E apt-get install -y git
 
 # install Node.js v5.x
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo -E apt-get install -y nodejs
 
 # install build-essential for Node modules w/native code
-sudo apt-get install -y build-essential
+sudo -E apt-get install -y build-essential
 
 # allow Node.js servers to bind to low ports
-sudo apt-get install -y chase
-sudo apt-get install -y libcap2-bin
-sudo setcap cap_net_bind_service=+ep $(chase $(which node))
+sudo -E apt-get install -y chase
+sudo -E apt-get install -y libcap2-bin
+sudo -E setcap cap_net_bind_service=+ep $(chase $(which node))
 
 # install MySQL
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+sudo -E apt-get install -y mysql-server
 
 # run the SQL commands from the mysql_secure_installation script
 # except for setting the root password (so that we don't embed)
@@ -35,15 +38,15 @@ FLUSH PRIVILEGES;
 EOF
 
 # install MongoDB and tools
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo -E apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo -E tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo -E apt-get update
+sudo -E apt-get install -y mongodb-org
 
 # install the kerberos library
 # which is needed to build the MongoDB
 # Node.js driver during npm install
-sudo apt-get install -y libkrb5-dev
+sudo -E apt-get install -y libkrb5-dev
 
 # install pm2 utility for managing node servers
-sudo npm install -g pm2 --loglevel error
+sudo -E npm install -g pm2 --loglevel error
