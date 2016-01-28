9fc21f3f7731db02f904846f7d966ff2ef844668
Dave Stearns <dlsinfo@uw.edu>
2016-01-27 19:37:49 -0800

    added comments, some cleanup and refactoring to reduce redundancy

--- node-db/controllers/stories-api.js
+++ node-db/controllers/stories-api.js
@@ -1,48 +1,73 @@
 'use strict';
 
-var express = require('express');
-var request = require('request');
-var cheerio = require('cheerio');
+var express = require('express');   //sub-routers 
+var request = require('request');   //request URLs
+var cheerio = require('cheerio');   //parse HTML
 
-module.exports.Router = function(Story) {
+module.exports.Router = function(stories) {
+    //create a new Express Router
+    //an Express application is a router, but we can also
+    //create sub-routers that we can add to the application
+    //this router will handle all routes related to stories
     var router = express.Router();
     
+    //GET /stories
     router.get('/stories', function(req, res, next) {
-        //return all stories from the database
-        Story.getAll()
+        //return the first page of stories from the database
+        //TODO: support offset and limit as query string parameters
+        //so the clients can page through all stories
+        stories.getAll()
             .then(function(rows) {
                 res.json(rows);
             })
             .catch(next);
     });
     
+    //POST /stories
     router.post('/stories', function(req, res, next) {
         //insert a new story into the database
         //and return the data with default values
         //applied
+        
+        //TODO: validate that req.body.url exists and is a valid URL
+        
+        //fetch the HTML for the new URL
         request.get(req.body.url, function(err, response, body) {
             if (err) {
+                //if there is a problem, just use the URL as the title
+                //and continue--articles behind a paywall or login
+                //are inaccessible to us
                 req.body.title = req.body.url;
             }
             else {
+                //parse the HTML using cheerio
+                //same API as jQuery once it's parsed
                 var $ = cheerio.load(body);
+                //grab the text of the title element within the head element
                 req.body.title = $('head title').text();                
             }
             
-            Story.insert(req.body)
+            //insert the new story
+            stories.insert(req.body)
                 .then(function(row) {
+                    //echo back the story with all default values applied
                     res.json(row);
                 })
-                .catch(next);            
+                .catch(next); //forwards the error to Express
         });
         
          
     });
     
+    //POST /stories/1234/votes
+    //the :id is like a wildcard--it matches any value
+    //and express will make the value it matches available as req.params.id
     router.post('/stories/:id/votes', function(req, res, next) {
         //upvote the story and return the
         //full story with current number of votes
-        Story.upVote(req.params.id)
+        //the property on req.params will have the same name as whatever
+        //you had following the : in the URL
+        stories.upVote(req.params.id)
             .then(function(row) {
                 res.json(row);
             })
--- node-db/server.js
+++ node-db/server.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var express = require('express');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var dbConfig = require('./secret/config-maria.json');
-var bluebird = require('bluebird');
+var morgan = require('morgan');                         //logging
+var bodyParser = require('body-parser');                //body parsing
+var mysql = require('mysql');                           //database
+var dbConfig = require('./secret/config-maria.json');   //database config
+var bluebird = require('bluebird');                     //promise wrapper
 
 //create a connection pool to the MariaDB server
 //this allow multiple queries to execute against
@@ -15,7 +15,7 @@ var connPool = bluebird.promisifyAll(mysql.createPool(dbConfig));
 //require our stories controller
 var storiesApi = require('./controllers/stories-api');
 //require our story model
-var Story = require('./models/story.js').Model(connPool);
+var stories = require('./models/stories.js').Model(connPool);
 
 //create the express application
 var app = express();
@@ -29,8 +29,9 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/static'));
 
 //mount the stories API router under /api/v1
-app.use('/api/v1', storiesApi.Router(Story));
+app.use('/api/v1', storiesApi.Router(stories));
 
+//start listening for HTTP requests on port 80
 app.listen(80, function() {
     console.log('server is listening...'); 
 });
