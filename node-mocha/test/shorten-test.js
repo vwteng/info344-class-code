dce2536fc21bc2be782cf6bbdbc13a16af241afe
Dave Stearns <dlsinfo@uw.edu>
2016-02-10 09:24:48 -0800

    halfway through class

--- node-mocha/test/shorten-test.js
+++ node-mocha/test/shorten-test.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var should = require('should');
+
+var shorten = require('../lib/shorten');
+
+describe('shorten module', function() {
+    it('should encode an id to a path', function() {
+        var enc = shorten.encode(99999);
+        should.exist(enc);
+        enc.length.should.be.above(0); 
+    });
+    
+    it('should decode a path back to the same id', function() {
+        var id = 99999;
+        shorten.decode(shorten.encode(id)).should.equal(id); 
+    });
+});
